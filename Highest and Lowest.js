27108fc74323ed66268b90de76969b1ea6ecf194
Tony <un_razum@mail.ru>
2022-09-07 22:16:15 +0300

    Create Highest and Lowest.js

---
+++ Highest and Lowest.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+function highAndLow(numbers){
+let nums = numbers.split(" ");
+  return `${Math.max(...nums)} ${Math.min(...nums)}`;
+}
