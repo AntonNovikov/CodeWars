cc150951103b7549a3377ea48b13925eabbef76a
Tony <un_razum@mail.ru>
2022-10-19 19:50:30 +0300

    Create altERnaTIng cAsE <=> ALTerNAtiNG CaSe.js

---
+++ 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.js
@@ -0,0 +1,15 @@
+String.prototype.toAlternatingCase = function () {
+ 
+let str = '';
+  for(let i = 0; i< this.length; i++){
+    if (this[i] == this[i].toLowerCase()){ 
+      str += this[i].toUpperCase();
+    } else {
+      str += this[i].toLowerCase();
+    }
+  }
+  return str;
+}
+ 
+
+// return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
