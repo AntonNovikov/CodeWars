8718abda389ecbc08b532c9e72b70f63ddc61988
Tony <un_razum@mail.ru>
2022-10-19 19:45:07 +0300

    Create Counting sheep....js

---
+++ 8kyu/Counting sheep....js
@@ -0,0 +1,17 @@
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  let result = 0;
+  arrayOfSheep.forEach((a) => {if(a)result++} )
+  
+return result
+}
+
+
+// function countSheeps(arrayOfSheeps) {
+//   return arrayOfSheeps.filter(Boolean).length;
+// }
+
+// function countSheeps(arrayOfSheep) {
+//   var array = arrayOfSheep.reduce((a,b)=>a + (b===true?1:0),0)
+//   return array;
+// }
