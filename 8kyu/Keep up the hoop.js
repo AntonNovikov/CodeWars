e602c62d8f00b7372150a1941e334cb23b4ed335
Tony <un_razum@mail.ru>
2022-10-22 16:04:41 +0300

    Create Keep up the hoop.js

---
+++ 8kyu/Keep up the hoop.js
@@ -0,0 +1,5 @@
+function hoopCount (n) {
+return n>=10?"Great, now move on to tricks":"Keep at it until you get it"
+  //your code goes here    
+}
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
