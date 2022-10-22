f24881a7512ddba3cff0af2d168ad3d3578fdd70
Tony <un_razum@mail.ru>
2022-10-22 14:35:53 +0300

    Create Powers of 2.js

---
+++ 8kyu/Powers of 2.js
@@ -0,0 +1,9 @@
+function powersOfTwo(n){
+  let res = []
+  for(let i = 0 ; i<=n;i++){
+    res.push(Math.pow(2,i))
+  }
+  return res
+}
+
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028/solutions/javascript
