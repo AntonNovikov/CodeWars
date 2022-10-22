6728bf17d5b87afcded5d3192e766b8ad0fc181e
Tony <un_razum@mail.ru>
2022-10-22 15:19:07 +0300

    Create Sum Mixed Array.js

---
+++ 8kyu/Sum Mixed Array.js
@@ -0,0 +1,9 @@
+function sumMix(x){
+return x.reduce((a,b)=>a*1+b*1)
+}
+
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/javascript
+
+// function sumMix(x){
+//  return x.reduce((s,x)=>s+ +x,0);
+// }
