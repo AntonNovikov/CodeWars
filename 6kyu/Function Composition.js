dd503d9436f51055bbc489d42de86b312354c19a
Tony <un_razum@mail.ru>
2023-01-28 23:39:45 +0300

    Update Function Composition.js

--- 6kyu/Function Composition.js
+++ 6kyu/Function Composition.js
@@ -1,7 +1,7 @@
-// https://www.codewars.com/kata/5421c6a2dda52688f6000af8
+// https://www.codewars.com/kata/5655c60db4c2ce0c2e000026
 
-function compose(f,g) {
-  return function(...a) {
-    return f(g(...a));
-  }
-}
+const compose =
+  (...funcs) =>
+  (arg) => {
+    return funcs.reduceRight((value, func) => func(value), arg);
+  };
