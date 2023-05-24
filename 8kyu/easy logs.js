cb311c4945218969676d8857314a84f213e2b126
Tony <un_razum@mail.ru>
2023-05-24 23:08:42 +0300

    Create easy logs.js

---
+++ 8kyu/easy logs.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5b68c7029756802aa2000176/train/javascript
+
+function logs(x , a, b){
+    //your code here
+      return Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x)
+    }
+
+
+    logs(10, 2, 3), 0.7781512503836435
+    logs(5, 2, 3), 1.1132827525593785
+    logs(5, 2, 3), 1.1132827525593785
